d1d5e4796f11604753bab2386f376d4e1b5a3eab
alexander-lam <alexander.lam@ansys.com>
2023-04-20 16:25:38 -0600

    Fix Potassium Furnace stoichiometry

--- user/+ASEN6116/+project/+components/K_Furnace_Manip.m
+++ user/+ASEN6116/+project/+components/K_Furnace_Manip.m
@@ -21,7 +21,7 @@ classdef K_Furnace_Manip < matter.manips.substance.stationary
             fCaOProduction = this.oMT.afMolarMass(this.oMT.tiN2I.CaO)/this.oMT.afMolarMass(this.oMT.tiN2I.CaF2)*afFlowRateIn(this.oMT.tiN2I.CaF2);
             fAlProduction = 2*this.oMT.afMolarMass(this.oMT.tiN2I.Al)/this.oMT.afMolarMass(this.oMT.tiN2I.AlF3)*afFlowRateIn(this.oMT.tiN2I.AlF3);
             fNa2OProduction = 2*this.oMT.afMolarMass(this.oMT.tiN2I.Na2O)/this.oMT.afMolarMass(this.oMT.tiN2I.NaF)*afFlowRateIn(this.oMT.tiN2I.NaF);
-            fTiProduction = this.oMT.afMolarMass(this.oMT.tiN2I.Ti)/this.oMT.afMolarMass(this.oMT.tiN2I.TiF4)*afFlowRateIn(this.oMT.tiN2I.TiF4);
+            fTiO2Production = this.oMT.afMolarMass(this.oMT.tiN2I.TiO2)/this.oMT.afMolarMass(this.oMT.tiN2I.TiF4)*afFlowRateIn(this.oMT.tiN2I.TiF4);
 
 
             % Stoichiometric consumption of K and O2
@@ -39,8 +39,9 @@ classdef K_Furnace_Manip < matter.manips.substance.stationary
 
             fO2Consumption = this.oMT.afMolarMass(this.oMT.tiN2I.O2)*...
                 (.5*fMgOProduction/this.oMT.afMolarMass(this.oMT.tiN2I.MgO)+...
-                .5*fCaOProduction/this.oMT.afMolarMass(this.oMT.tiN2I.MgO)+...
-                .5*fNa2OProduction/this.oMT.afMolarMass(this.oMT.tiN2I.Na2O));
+                .5*fCaOProduction/this.oMT.afMolarMass(this.oMT.tiN2I.CaO)+...
+                .5*fNa2OProduction/this.oMT.afMolarMass(this.oMT.tiN2I.Na2O)+...
+                fTiO2Production/this.oMT.afMolarMass(this.oMT.tiN2I.TiO2));
 
             % Set output flows
             afPartialFlowRates = zeros(1, this.oPhase.oMT.iSubstances);
@@ -50,16 +51,19 @@ classdef K_Furnace_Manip < matter.manips.substance.stationary
             afPartialFlowRates(this.oMT.tiN2I.CaO) = fCaOProduction;
             afPartialFlowRates(this.oMT.tiN2I.Al) = fAlProduction;
             afPartialFlowRates(this.oMT.tiN2I.Na2O) = fNa2OProduction;
-            afPartialFlowRates(this.oMT.tiN2I.Ti) = fTiProduction;
+            afPartialFlowRates(this.oMT.tiN2I.TiO2) = fTiO2Production;
 
+            afPartialFlowRates(this.oMT.tiN2I.FeF3) = -afFlowRateIn(this.oMT.tiN2I.FeF3);
+            afPartialFlowRates(this.oMT.tiN2I.MgF2) = -afFlowRateIn(this.oMT.tiN2I.MgF2);
+            afPartialFlowRates(this.oMT.tiN2I.CaF2) = -afFlowRateIn(this.oMT.tiN2I.CaF2);
+            afPartialFlowRates(this.oMT.tiN2I.AlF3) = -afFlowRateIn(this.oMT.tiN2I.AlF3);
+            afPartialFlowRates(this.oMT.tiN2I.NaF) = -afFlowRateIn(this.oMT.tiN2I.NaF);
+            afPartialFlowRates(this.oMT.tiN2I.TiF4) = -afFlowRateIn(this.oMT.tiN2I.TiF4);
+            afPartialFlowRates(this.oMT.tiN2I.K) = -fKConsumption;
+            afPartialFlowRates(this.oMT.tiN2I.F2) = -fFConsumption/2;
+            afPartialFlowRates(this.oMT.tiN2I.O2) = -fO2Consumption;
 
-            % Account for excess fluorine
-            afPartialFlowRates(this.oMT.tiN2I.K) = afFlowRateIn(this.oMt.tiN2I.K) - fKConsumption;
-
-            afPartialFlowRates(this.oMT.tiN2I.O2) = afFlowRateIn(this.oMt.tiN2I.O2) - fO2Consumption;
-
-
-            % Any unaccounted mass is oxygen
+            % Any unaccounted mass is KF
             afPartialFlowRates(this.oMT.tiN2I.KF) = -sum(afPartialFlowRates);
 
             update@matter.manips.substance.stationary(this, afPartialFlowRates);
